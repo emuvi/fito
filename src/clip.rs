fbcbfcae50cfd8c033a9171c3acdb7021b4fcfe8
Éverton M. Vieira <everton.muvi@gmail.com>
2022-02-18 18:01:11 -0300

    [Release] version 0.1.1

--- src/clip.rs
+++ src/clip.rs
@@ -1,16 +1,16 @@
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{Arg, ArgMatches, Command};
 
-pub fn parse<'a>() -> ArgMatches<'a> {
-    App::new("fito")
+pub fn parse() -> ArgMatches {
+    Command::new("fitx")
         .version(clap::crate_version!())
         .author("Éverton M. Vieira <everton.muvi@gmail.com>")
         .about("Fitx (File System Toolbox) is a library and a command program that features a toolbox with a series of file system functionalities.")
         .subcommand(
-            SubCommand::with_name("compare")
+            Command::new("compare")
                 .about("Compares two path, looking for differences.")
                 .arg(
-                    Arg::with_name("input")
-                        .short("i")
+                    Arg::new("input")
+                        .short('i')
                         .long("input")
                         .value_name("PATH")
                         .takes_value(true)
@@ -18,8 +18,8 @@ pub fn parse<'a>() -> ArgMatches<'a> {
                         .help("The path origin for comparison."),
                 )
                 .arg(
-                    Arg::with_name("output")
-                        .short("o")
+                    Arg::new("output")
+                        .short('o')
                         .long("output")
                         .value_name("PATH")
                         .takes_value(true)
@@ -27,35 +27,35 @@ pub fn parse<'a>() -> ArgMatches<'a> {
                         .help("The path destiny for comparison."),
                 )
                 .arg(
-                    Arg::with_name("only-diffs")
+                    Arg::new("only-diffs")
                         .long("only-diffs")
                         .takes_value(false)
                         .required(false)
                         .help("Prints only the differences."),
                 )
                 .arg(
-                    Arg::with_name("check-size")
+                    Arg::new("check-size")
                         .long("check-size")
                         .takes_value(false)
                         .required(false)
                         .help("Checks for the size of the files."),
                 )
                 .arg(
-                    Arg::with_name("check-created")
+                    Arg::new("check-created")
                         .long("check-created")
                         .takes_value(false)
                         .required(false)
                         .help("Checks for the created time of the files."),
                 )
                 .arg(
-                    Arg::with_name("check-modified")
+                    Arg::new("check-modified")
                         .long("check-modified")
                         .takes_value(false)
                         .required(false)
                         .help("Checks for the modified time of the files."),
                 )
                 .arg(
-                    Arg::with_name("check-accessed")
+                    Arg::new("check-accessed")
                         .long("check-accessed")
                         .takes_value(false)
                         .required(false)
