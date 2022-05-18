fc374f3638f16aa298e05e7126a3946e64be4fe9
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-18 10:59:56 -0300

    refactor of the clip compare command to a function

--- src/clip.rs
+++ src/clip.rs
@@ -5,62 +5,64 @@ pub fn parse() -> ArgMatches {
     .version(clap::crate_version!())
     .author("Éverton M. Vieira <emuvi@outlook.com.br>")
     .about("Fitx (File System Toolbox) is a library and a command program that features a toolbox with a series of file system functionalities.")
-    .subcommand(
-      Command::new("compare")
-        .about("Compares two path, looking for differences.")
-        .arg(
-          Arg::new("side-a")
-            .short('a')
-            .long("side-a")
-            .value_name("PATH")
-            .takes_value(true)
-            .required(true)
-            .help("The path origin for comparison."),
-        )
-        .arg(
-          Arg::new("side-b")
-            .short('b')
-            .long("side-b")
-            .value_name("PATH")
-            .takes_value(true)
-            .required(true)
-            .help("The path destiny for comparison."),
-        )
-        .arg(
-          Arg::new("only-diffs")
-            .long("only-diffs")
-            .takes_value(false)
-            .required(false)
-            .help("Prints only the differences."),
-        )
-        .arg(
-          Arg::new("check-size")
-            .long("check-size")
-            .takes_value(false)
-            .required(false)
-            .help("Checks for the size of the files."),
-        )
-        .arg(
-          Arg::new("check-created")
-            .long("check-created")
-            .takes_value(false)
-            .required(false)
-            .help("Checks for the created time of the files."),
-        )
-        .arg(
-          Arg::new("check-modified")
-            .long("check-modified")
-            .takes_value(false)
-            .required(false)
-            .help("Checks for the modified time of the files."),
-        )
-        .arg(
-          Arg::new("check-accessed")
-            .long("check-accessed")
-            .takes_value(false)
-            .required(false)
-            .help("Checks for the accessed time of the files."),
-        ),
-    )
+    .subcommand(cmd_compare())
     .get_matches()
+}
+
+fn cmd_compare<'a>() -> Command<'a> {
+  Command::new("compare")
+    .about("Compares two path, looking for differences.")
+    .arg(
+      Arg::new("side-a")
+        .short('a')
+        .long("side-a")
+        .value_name("PATH")
+        .takes_value(true)
+        .required(true)
+        .help("The path origin for comparison."),
+    )
+    .arg(
+      Arg::new("side-b")
+        .short('b')
+        .long("side-b")
+        .value_name("PATH")
+        .takes_value(true)
+        .required(true)
+        .help("The path destiny for comparison."),
+    )
+    .arg(
+      Arg::new("only-diffs")
+        .long("only-diffs")
+        .takes_value(false)
+        .required(false)
+        .help("Prints only the differences."),
+    )
+    .arg(
+      Arg::new("check-size")
+        .long("check-size")
+        .takes_value(false)
+        .required(false)
+        .help("Checks for the size of the files."),
+    )
+    .arg(
+      Arg::new("check-created")
+        .long("check-created")
+        .takes_value(false)
+        .required(false)
+        .help("Checks for the created time of the files."),
+    )
+    .arg(
+      Arg::new("check-modified")
+        .long("check-modified")
+        .takes_value(false)
+        .required(false)
+        .help("Checks for the modified time of the files."),
+    )
+    .arg(
+      Arg::new("check-accessed")
+        .long("check-accessed")
+        .takes_value(false)
+        .required(false)
+        .help("Checks for the accessed time of the files."),
+    )
 }
